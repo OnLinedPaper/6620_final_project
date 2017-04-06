362ce28e9a540cb968684862be184d59ecbf4ddc
Nicholas Reed <ndreed@g.clemson.edu>
2017-04-06 18:34:00 -0400

    Signin.html: added comments to make the document easier to read h helloworld.php: added elements to catch and display the user's input

--- helloworld.php
+++ helloworld.php
@@ -1,3 +1,10 @@
-<?php
-    echo "you have summoned the php action file!\n";
-?>
+<html>
+<body>
+
+    Entered username: <?php echo $_POST['uname']; ?><br>
+    Entered email: <?php echo $_POST['email']; ?><br>
+    Entered password: <?php echo $_POST['pword']; ?>&nbsp
+    (This will not be visible in the final implementation)
+
+</body>
+</html>
