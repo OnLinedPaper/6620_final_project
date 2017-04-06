ed2182b279ebdcc9a4fce630717fd0109028c828
Nicholas Reed <ndreed@g.clemson.edu>
2017-04-06 18:19:57 -0400

    altered helloworld.php for testing purposes (seeing if the html form will call the php file)

--- helloworld.php
+++ helloworld.php
@@ -1,20 +1,3 @@
 <?php
-    echo "hello world!\n";
-    echo "i am going to try to connect to a database.\n\n";
-
-    echo "first, i will create some variables...\n";
-    $mysql_host = 'mysql1.cs.clemson.edu';
-    $mysql_user = 'metube_8ma5';
-    $mysql_password = 'metube6620';
-    $my_database = 'metube_wlbj';
-
-/*    echo "\ndone. great! now, i will try to connect.\n";
-    $link = mysql_connect($mysql_host, $mysql_user, $mysql_password, $my_database) or die('could not connect: '.mysql_error($link));
-
-
-    echo "now i will close the connection.\n";
-    mysql_close($link);
-*/
-
-//    phpinfo();
+    echo "you have summoned the php action file!\n";
 ?>
