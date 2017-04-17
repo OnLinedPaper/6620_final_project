e88c0f3202c2528dd443873a15fce41b63fffaab
Nicholas Reed <ndreed@g.clemson.edu>
2017-04-16 23:40:16 -0400

    made minor changes to media player

--- media.php
+++ media.php
@@ -35,7 +35,7 @@ if(isset($_GET['id'])) {
     {
 ?>
     <!-- <p>Viewing Video:<?php echo $result_row[2].$result_row[1];?></p> -->
-    <p>Viewing Video:<?php echo $result_row[4];?></p>
+    <p>Viewing Video: <?php echo $filename;?></p>
 
     <object id="MediaPlayer" width=320 height=286 classid="CLSID:22D6f312-B0F6-11D0-94AB-0080C74C7E95" standby="Loading Windows Media Player components…" type="application/x-oleobject" codebase="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=6,4,7,1112">
 
