c1ae2f344ce9a6abbc80fde823da3399b08c1c1e
Nicholas Reed <ndreed@g.clemson.edu>
2017-04-16 23:20:30 -0400

    changed some varaible names (result_row >> result)

--- media.php
+++ media.php
@@ -28,14 +28,14 @@ if(isset($_GET['id'])) {
     if(substr($type,0,5)=="image") //view image
     {
         echo "Viewing Picture:";
-        echo $result_row[4];
+        echo $result[4];
         echo "<img src='".$filepath."'/>";
     }
     else //view movie
     {
 ?>
-    <!-- <p>Viewing Video:<?php echo $result_row[2].$result_row[1];?></p> -->
-    <p>Viewing Video:<?php echo $result_row[4];?></p>
+    <!-- <p>Viewing Video:<?php echo $result[2].$result[1];?></p> -->
+    <p>Viewing Video:<?php echo $result[4];?></p>
 
     <object id="MediaPlayer" width=320 height=286 classid="CLSID:22D6f312-B0F6-11D0-94AB-0080C74C7E95" standby="Loading Windows Media Player components…" type="application/x-oleobject" codebase="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=6,4,7,1112">
 
