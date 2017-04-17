d2523e17d6a6fa01b0f17c1c153a21fbec8e59b4
Nicholas Reed <ndreed@g.clemson.edu>
2017-04-16 23:47:14 -0400

    removed old media player

--- media.php
+++ media.php
@@ -37,18 +37,19 @@ if(isset($_GET['id'])) {
     <!-- <p>Viewing Video:<?php echo $result_row[2].$result_row[1];?></p> -->
     <p>Viewing Video: <?php echo $filename;?></p>
 
-    <object id="MediaPlayer" width=320 height=286 classid="CLSID:22D6f312-B0F6-11D0-94AB-0080C74C7E95" standby="Loading Windows Media Player components…" type="application/x-oleobject" codebase="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=6,4,7,1112">
+<!--    <object id="MediaPlayer" width=320 height=286 classid="CLSID:22D6f312-B0F6-11D0-94AB-0080C74C7E95" standby="Loading Windows Media Player components…" type="application/x-oleobject" codebase="http://activex.microsoft.com/activex/controls/mplayer/en/nsmp2inf.cab#Version=6,4,7,1112">
 
 <param name="filename" value="<?php echo $result_row[1];?>">
-    <!-- echo $result_row[2].$result_row[1];  -->
+
+     echo $result_row[2].$result_row[1];  -->
 
 
-<param name="Showcontrols" value="True">
+<!--<param name="Showcontrols" value="True">
 <param name="autoStart" value="True">
 
 <embed type="application/x-mplayer2" src="<?php echo $filepath;  ?>" name="MediaPlayer" width=320 height=240></embed>
 
-</object>
+</object>-->
 
 <video width="320" height="240" controls>
     <source src=<?php echo "'$filepath'";?> type=<?php echo "'$type'";?>>
