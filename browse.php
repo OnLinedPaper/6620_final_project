6de5ea41541400e001cc8ef402836e796015741a
Nicholas Reed <ndreed@g.clemson.edu>
2017-04-17 15:17:38 -0400

    attempting to redirect not-signed-in users

--- browse.php
+++ browse.php
@@ -30,6 +30,7 @@ function saveDownload(id)
             echo "uh-oh";
             ?>
             <meta http-equiv="refresh" content="0; url=http://webapp.cs.clemson.edu/~ndreed/metube/" />
+            <script>window.stop()</script>
             <?php
         }
     ?>
