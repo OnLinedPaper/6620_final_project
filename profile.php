321f647f758eab5afe7642dd4ff56701f0683da2
Nicholas Reed <ndreed@g.clemson.edu>
2017-04-18 13:34:54 -0400

    creating profile page

--- profile.php
+++ profile.php
@@ -13,7 +13,7 @@
         }
 
         if(isset($_GET['id'])){
-            $query = "SELECT account.account_id, account.username, media.media_id, media.name FROM account JOIN media ON account.account_id = media.account_id WHERE account.account_id = ".$_GET['id'];
+            $query = "SELECT account.account_id, account.username, media.media_id, media.name FROM account LEFT JOIN media ON account.account_id = media.account_id WHERE account.account_id = ".$_GET['id'];
             $result = mysql_query($query);
             $result_row = mysql_fetch_row($result);
             #account_id, username, media id, medianame
