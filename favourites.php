014a9e98dd0cfeb6c19323b2eb5b133df2a9c078
ash-aras <ash.aras25@gmail.com>
2017-04-19 19:49:54 -0400

    Update favourites.php

--- favourites.php
+++ favourites.php
@@ -12,67 +12,67 @@ session_start();
 <body>
 
 
-<!-- This section displays the favourite media -->
-<?php
-
-    $query = "SELECT favourite.media_id, media.name, account.account_id, account.username, media.type, interaction.media_blocked ";
-    $query = $query."FROM Favourite ";
-    $query = $query."JOIN account ON favourite.account_id = account.account_id ";
-	$query = $query."JOIN media ON favourite.media_id = media.media_id ";
-    $query = $query."LEFT JOIN interaction ON (favourite.account_id = interaction.account_id AND ".$_SESSION['account_id']." = interaction.target_id);";
-    $result = mysql_query( $query );
-    #favourite media_id, name, account_id, username, type
-    if (!$result){
-       die ("Could not query the favourites table in the database : <br />". mysql_error());
-    }
-
-?>
-
-
-<form method="post" action="favourites_process.php">
-	<p style="margin:0; padding:0">
-		<h2>Your Favourites</h2>
-	</p>
-</form>
-
-
-<!-- Display favourite media as a table of links and IDs -->
-<table width="50%" cellpadding="0" cellspacing="0">
+	<!-- This section displays the favourite media -->
 	<?php
-		while ($result_row = mysql_fetch_row($result))
-		#media_id, name, path, account_id, username
-		{
-			$mediaid = $result_row[0];
-			$filename = $result_row[1];
-			$uploader_id = $result_row[2];
-			$uploader_username = $result_row[3];
-			$type = $result_row[4];
-			$blocked = $result_row[5];
 
-			if(!$blocked and ($category=="all" or substr($type,0,5)==$category or ($category=="other" and substr($type,0,5) != "video" and substr($type,0,5) != "image")))
+    		$query = "SELECT favourite.media_id, media.name, account.account_id, account.username, media.type, interaction.media_blocked ";
+    		$query = $query."FROM favourite ";
+    		$query = $query."JOIN account ON favourite.account_id = account.account_id ";
+		$query = $query."JOIN media ON favourite.media_id = media.media_id ";
+    		$query = $query."LEFT JOIN interaction ON (favourite.account_id = interaction.account_id AND ".$_SESSION['account_id']." = interaction.target_id);";
+    		$result = mysql_query( $query );
+    		#favourite media_id, name, account_id, username, type
+    		if (!$result){
+       			die ("Could not query the favourites table in the database : <br />". mysql_error());
+    		}
+
+	?>
+
+
+	<form method="post" action="favourites_process.php">
+		<p style="margin:0; padding:0">
+			<h2>Your Favourites</h2>
+		</p>
+	</form>
+
+
+	<!-- Display favourite media as a table of links and IDs -->
+	<table width="50%" cellpadding="0" cellspacing="0">
+		<?php
+			while ($result_row = mysql_fetch_row($result))
+			#media_id, name, path, account_id, username
 			{
-	?>
+				$mediaid = $result_row[0];
+				$filename = $result_row[1];
+				$uploader_id = $result_row[2];
+				$uploader_username = $result_row[3];
+				$type = $result_row[4];
+				$blocked = $result_row[5];
+
+				if(!$blocked and ($category=="all" or substr($type,0,5)==$category or ($category=="other" and substr($type,0,5) != "video" and substr($type,0,5) != "image")))
+				{
+		?>
 	
-				<tr valign="top">
-					<td>
-						<?php
-							echo $mediaid;  //mediaid
-						?>
-					</td>
-					<td>
-						<!--display file name-->
-						<!--this link takes us to media.php, with the media id sent via GET for media.php to use-->
-						<a href="media.php?id=<?php echo $mediaid;?>" target="_blank"><?php echo $filename;?></a>
-					</td>
-					<td>
-						<a href="profile.php?id=<?php echo $uploader_id ?>"><?php echo $uploader_username; ?></a>
-					</td>
-				</tr>
+					<tr valign="top">
+						<td>
+							<?php
+								echo $mediaid;  //mediaid
+							?>
+						</td>
+						<td>
+							<!--display file name-->
+							<!--this link takes us to media.php, with the media id sent via GET for media.php to use-->
+							<a href="media.php?id=<?php echo $mediaid;?>" target="_blank"><?php echo $filename;?></a>
+						</td>
+						<td>
+							<a href="profile.php?id=<?php echo $uploader_id ?>"><?php echo $uploader_username; ?></a>
+						</td>
+					</tr>
 				<?php
+				}
 			}
-		}
-	?>
-</table>
+		?>
+	</table>
 
 </body>
 </html>
