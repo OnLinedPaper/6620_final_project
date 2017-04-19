c60b164665d4491aba129a347c7c007cea485c4e
Nicholas Reed <ndreed@g.clemson.edu>
2017-04-19 13:43:46 -0400

    updated create_table to refelct addition of comments and interaction tables

--- create_table.sql
+++ create_table.sql
@@ -7,9 +7,7 @@ CREATE TABLE media(media_id INT NOT NULL AUTO_INCREMENT, name VARCHAR(200), type
 CREATE TABLE downloads(download_id INT NOT NULL AUTO_INCREMENT, account_id INT, media_id INT, ip INT, download_time TIME, PRIMARY KEY (download_id), FOREIGN KEY (account_id) REFERENCES account(account_id), FOREIGN KEY (media_id) REFERENCES media(media_id));
     /* download_id, account_id, media_id, ip, download_time */
 
+CREATE TABLE interaction(account_id INT, target_id INT, contact TINYINT(1), friend TINYINT(1), foe TINYINT(1), blocked TINYINT(1), media_blocked TINYINT(1), FOREIGN KEY (account_id) REFERENCES account(account_id), FOREIGN KEY (target_id) REFERENCES account(account_id));
+    /* account_id, target_id, contact, friend, foe, blocked, media_blocked */
 
-/*create table account (account_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, type INT, username VARCHAR(40), password VARCHAR(40), email VARCHAR(40));*/
-
-/*create table media (media_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(200), type VARCHAR(20), path VARCHAR(200), last_access_time TIME, account_id INT FOREIGN KEY REFERENCES account(account_id), ip INT, upload_time TIME);*/
-
-/*create table downloads (download_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, account_id INT FOREIGN KEY REFERENCES account(account_id), media_id INT FOREIGN KEY REFERENCES media(media_id), ip INT, download_time TIME);*/
+CREATE TABLE comments(media_id INT, account_id INT, comment VARCHAR(2000), FOREIGN KEY (media_id) REFERENCES media(media_id), FOREIGN KEY account_id REFERENCES account(account_id));
