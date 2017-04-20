ee375ea7b4d367eaca79c8e8ffe145b69fb3dec5
Nicholas Reed <ndreed@g.clemson.edu>
2017-04-20 12:31:02 -0400

    final cleanup and validation

--- update_profile.php
+++ update_profile.php
@@ -40,7 +40,7 @@
         {
             echo "That username is taken already.";
         }
-        else if(!strpos("@", $_POST['email'])){
+        else if(strpos("@", $_POST['email']) == false){
             echo "<b>email is invalid</b><br />";
         }
         else{
